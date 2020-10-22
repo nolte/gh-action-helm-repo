a00ceab95a40b54f64f5934763e50583f4787f91
nolte <nolte07@googlemail.com>
2020-10-22 23:59:40 +0200

    add

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 
 const core = require('@actions/core');
+const yaml = require('js-yaml');
+const fs = require('fs');
+const https = require('https');
+
 
 async function main() {
+    const repo = core.getInput('repo', { required: true });
+    let myURL = new URL(repo + '/index.yaml');
+    let body = [];
+    https.request(myURL, res => {
+        // XXX verify HTTP 200 response
+        res.on('data', chunk => body.push(chunk));
+        res.on('end', () => console.log(Buffer.concat(body).toString()));
+    }).end()
+
+    const doc = yaml.safeLoad(Buffer.concat(body).toString(), 'utf8');
+    console.log(doc);
     console.log("Test App")
+
 
 }
 main().catch((e) => core.setFailed(e.message));
